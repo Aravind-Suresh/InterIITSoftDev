fe7a0a00662405fbb28bd8f0f25a8df6fc6de2b3
aravind <arvindsuresh2009@gmail.com>
2016-01-13 01:07:16 +0530

    Minor fix to parser.

--- retreive.py
+++ retreive.py
@@ -4,11 +4,13 @@ import argparse
 from PIL import Image
 
 def getDetails(rollNumber):
+    details = {}
+    # TODO: Parse hostel CCW page and store results
     return details
 
 def getImage(rollNumber):
     url = "https://photos.iitm.ac.in/byroll.php?roll=" + rollNumber.upper()
-    filePath = "images/" + rollNumber + ".jpg"
+    filePath = os.getcwd() + os.sep + "data" + os.sep + "images" + os.sep + rollNumber + ".jpg"
     urllib.urlretrieve(url, filePath)
     return filePath
 
@@ -17,28 +19,35 @@ def showImage(filePath):
     img.show()
 
 def displayData(details):
-    # Create window to display data
+    # TODO: Create window to display data
+    showImage(details['__image_path__'])
 
-def main():
+if __name__ == '__main__':
+    # Parsing arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-r','--roll_number', help='Roll number', required=True)
     parser.add_argument('-s','--save', help='Save data')
     args = parser.parse_args()
 
     toSave = False
-    if(args['save']):
+    if args.save:
         toSave = True
 
-    rollNumber = args['roll_number']
+    rollNumber = args.roll_number.upper()
 
     details = getDetails(rollNumber)
     imagePath = getImage(rollNumber)
-    details['__image_path__'] = os.getcwd() + os.sep + imagePath
+    details['__image_path__'] = imagePath
 
+    # Displaying student details and showing image
     displayData(details)
 
+    if toSave:
+        # Saving details to json file
+        outFilePath = os.getcwd() + os.sep + "data" + os.sep + "details" + os.sep + rollNumber + ".json"
+        with open(outFilePath, 'w') as outfile:
+            json.dump(details, outfile)
 
-
-
-if __name__ == '__main__':
-    main()
+    else:
+        # Removing the image file which was retrieved earlier
+        os.rmdir(imagePath)
